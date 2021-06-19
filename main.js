e0eeb5692fca138e78484abd375c4190f681af80
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2021-06-19 15:37:23 -0700

    add sleeps

--- main.js
+++ main.js
@@ -178,6 +178,8 @@ async function unsendAllVisibleMessages(lastRun, count) {
             el.remove();
           });
           unsend_buttons.length = 0;
+          console.log("also sleeping for 30s");
+          await sleep(30000);
         }
       }
 
@@ -188,6 +190,8 @@ async function unsendAllVisibleMessages(lastRun, count) {
         });
         console.log("too many removes, skipping");
         remove_buttons.length = 0;
+        console.log("also sleeping for 30s");
+        await sleep(30000);
       }
     }
     more_buttons = [...document.querySelectorAll(MORE_BUTTONS_QUERY)].filter(
